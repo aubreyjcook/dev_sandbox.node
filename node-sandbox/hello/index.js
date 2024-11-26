a0050e75ee1993d3e102042177a1638db151f193
aubreyjcook <aubreyjcook@gmail.com>
2024-11-26 14:39:23 -0600

    update repo >hello init;

---
+++ node-sandbox/hello/index.js
@@ -0,0 +1,16 @@
+// index.js
+require('dotenv').config();
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, Node.js Sandbox!\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
